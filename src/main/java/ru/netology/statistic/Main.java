46016fd59a65ec9d46f2958c2dd944ee44694a13
aleksandr.b <ser.sash@gmail.com>
2024-06-08 15:54:09 +0500

    second commit

---
+++ src/main/java/ru/netology/statistic/Main.java
@@ -0,0 +1,15 @@
+package ru.netology.statistic;
+
+public class Main {
+    public static void main(String[] args) {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+
+    }
+
+}
